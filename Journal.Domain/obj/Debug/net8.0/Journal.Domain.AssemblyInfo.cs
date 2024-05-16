f4eccd0211add09564d082bdaa18558dd8f53638
m1xar <mstyvola@gmail.com>
2024-05-16 12:37:26 +0300

    New origin

--- Journal.Domain/obj/Debug/net8.0/Journal.Domain.AssemblyInfo.cs
+++ Journal.Domain/obj/Debug/net8.0/Journal.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Journal.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1c8438df4b93461468bcae72b6464fb477b997")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Journal.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Journal.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
