e6efae36ba1c2886210b090add00ec703b34e2b8
m1xar <mstyvola@gmail.com>
2024-05-23 14:00:23 +0300

    New calls

--- Journal/obj/Debug/net8.0/Journal.AssemblyInfo.cs
+++ Journal/obj/Debug/net8.0/Journal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Journal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e62e9c18cce3d97b2f059def4d4c9221bcb1abb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd545316638ad9817777a3d2664aad1dac8c7b29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Journal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Journal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
